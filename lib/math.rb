f7c12cbec5e7936f7d6c99ee957a50070e138688
Erik Zhao <erikzhao98@gmail.com>
2020-07-18 22:02:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,38 +1,24 @@
-require "pry"
 def addition(num1, num2)
-  return "#{num1} + #{num2}"
+  num1+num2
 end
-
-addition(5,4)
-
-binding.pry
 
 def subtraction(num1, num2)
-  puts "#{num1} - #{num2}"
+  num1-num2
 end
-
-subtraction(10,5)
 
 def division(num1, num2)
-  puts "#{num1} /  #{num2}"
+  num1/num2
 end
-
-division(50,2)
 
 def multiplication(num1, num2)
-  puts "#{num1} * #{num2}"
+  num1*num2
 end
-
-multiplication(4,30)
 
 def modulo(num1, num2)
-  puts "#{num1}.modulo(#{num2})"
+  num1.modulo(num2)
 end
-
-modulo(34,5)
 
 def square_root(num)
-  puts "math.sqrt(num)"
+  math.sqrt(81)
 end
 
-square_root(81)
